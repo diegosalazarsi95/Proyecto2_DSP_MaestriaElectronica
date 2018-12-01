40dcd81af69994219065d59472052b9519e30760
esolera <edgarasolera994@gmail.com>
2018-12-01 13:52:58 -0600

    Cambio por copand

--- funciones_compresor/compresion.m
+++ funciones_compresor/compresion.m
@@ -1,13 +1,3 @@
 function resultado = compresion(data, parameter)
-  resultado=zeros(1,length(data));
-  denominador=log(1+parameter);
-  for i =1:length(data)
-    if(data(i)<0)
-      numerador=log(1+(parameter*abs(data(i))));
-      resultado(i)=-numerador/denominador;
-    else
-    numerador=log(1+(parameter*abs(data(i))));
-    resultado(i)=numerador/denominador;
-    end
-  endfor
+  resultado=compand(data,parameter,1,"mu/compressor");
 endfunction
